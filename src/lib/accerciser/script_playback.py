821f6c00ab1ae14510dcf08a69cd8de64a962612
Eitan Isaacson <eitani@src.gnome.org>
2007-04-10 20:20:59 +0000

    Added run(). Made it all a bit more robust.
    * src/lib/accerciser/script_playback.py: Added run(). Made it all
    a bit more robust.
    
    svn path=/trunk/; revision=58

--- src/lib/accerciser/script_playback.py
+++ src/lib/accerciser/script_playback.py
@@ -1,13 +1,13 @@
 import sys
 from time import sleep, time
 import wnck
-import gtk
+import gobject, gtk
 from re import split
 import pyLinAcc
+import os
 
-sleep_time = 0.2
-focus_retry = 5
-focus_retry_time = 0.4
+keystroke_interval = 0.2
+focus_timeout = 5
 
 eg = pyLinAcc.Registry.getDeviceEventController()
 
@@ -35,53 +35,72 @@ keySymAliases = {
     '\n' : 'Return'
 }
 
+class _WindowManager:
+  def __init__(self):
+    self.loop = None
+    self.screen = wnck.screen_get_default()
+  def getApp(self, app_name):
+    self.loop = gobject.MainLoop()
+    self.returned_app = None
+    self.screen.connect('application-opened', self._onAppOpened, app_name)
+    gobject.idle_add(self._onIdleGetApp, app_name)
+    gobject.timeout_add(focus_timeout*1000, self.loop.quit)
+    self.loop.run()
+    return self.returned_app
+  def _onIdleGetApp(self, app_name):
+    for window in self.screen.get_windows():
+      app = window.get_application()
+      if app.get_name() == app_name:
+        self.returned_app = app
+        self.loop.quit()
+    return False
+  def _onAppOpened(self, screen, app, app_name):
+    if app.get_name() == app_name:
+      self.returned_app = app
+      self.loop.quit()
+  def focusFrame(self, app, frame_name):
+    window = None
+    for w in app.get_windows():
+      if w.get_name() == frame_name:
+        window = w
+        break
+    if not window:
+      return False
+    window.activate(0)
+    self.loop = gobject.MainLoop()
+    self.activate_success = False
+    self.screen.connect('active-window-changed', 
+                        self._onWindowActivated, window)
+    gobject.idle_add(self._onIdleGetApp, window)
+    gobject.timeout_add(focus_timeout*1000, self.loop.quit)
+    self.loop.run()
+  def _onIdleGetApp(self, window):
+    if self.screen.get_active_window() == window:
+      self.activate_success = True
+      self.loop.quit()
+  def _onWindowActivated(self, screen, window):
+    if self.screen.get_active_window() == window:
+      self.activate_success = True
+      self.loop.quit()
+
+
 class Focus:
   def __init__(self):
     self.current_app = None
     self.current_frame = None
     self.node = pyLinAcc.Registry.getDesktop(0)
-    self._getApps()
-
-  def _getApps(self):
-    window_list = []
-    try_limit = 50
-    while not window_list and try_limit:
-      gtk.main_iteration(False)
-      wnck_screen = wnck.screen_get_default()
-      window_list = wnck_screen.get_windows_stacked()
-      try_limit -= 1
-    if not try_limit and not self.window_list:
-      raise RuntimeError('Could not get window list')
-    self.apps = set([w.get_application() for w in window_list])
+    self.wm = _WindowManager()
+    self.dialog = self.frame
 
   def application(self, app_name):
-    for try_num in xrange(focus_retry):
-      for app in self.apps:
-        if app.get_name() == app_name:
-          self.current_app = app
-          return True
-      sleep(focus_retry_time)
-      self._getApps()
-    return False
+    self.current_app = self.wm.getApp(app_name)
+    success = bool(self.current_app)
+    return success
 
   def frame(self, frame_name):
-    if not self.current_app:
-      raise Exception('No application was focused')
-    for window in self.current_app.get_windows():
-      if window.get_name() == frame_name:
-        self.current_frame = window
-        self.current_frame.activate(0)
-        gtk.main_iteration(False)
-        return True
-    return False
-
-  def _findAcc(self, acc_name):
-    for child in self.node:
-      if child.name == acc_name:
-        self.node = child
-        return True
-    return False
-    
+    self.current_frame = self.wm.focusFrame(self.current_app, frame_name)
+    success = bool(self.current_frame)
+    return success
 
 def keyCombo(keystring):
   key_combo = []
@@ -96,13 +115,13 @@ def pressKeys(keys):
   modifiers = map(ModifierKeyCodes.get, keys[:-1])
   finalKey = keys[-1]
   for key_code in modifiers:
-    sleep(sleep_time)
+    sleep(keystroke_interval)
     eg.generateKeyboardEvent(key_code, '', pyLinAcc.Constants.KEY_PRESS)
-  sleep(sleep_time)
+  sleep(keystroke_interval)
   eg.generateKeyboardEvent(_charToKeySym(finalKey), 
                            '', pyLinAcc.Constants.KEY_SYM)
   for key_code in modifiers:
-    sleep(sleep_time)
+    sleep(keystroke_interval)
     eg.generateKeyboardEvent(key_code, '', pyLinAcc.Constants.KEY_RELEASE)
 
 def _charToKeySym(key):
@@ -115,7 +134,15 @@ def _charToKeySym(key):
 def type(text):
   text_syms = map(_charToKeySym, text)
   for key in text_syms:
-    sleep(sleep_time)
+    sleep(keystroke_interval)
     eg.generateKeyboardEvent(key, '', pyLinAcc.Constants.KEY_SYM)
     
 focus = Focus()
+
+def run(cmd, arguments = '', appName=''):
+  if arguments:
+    args = arguments.split(' ')
+  else:
+    args = []
+  os.spawnlp(os.P_NOWAIT, cmd, cmd, *args)
+  return focus.application(appName or cmd)
