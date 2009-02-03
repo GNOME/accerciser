03ee61b3ff39a5f8369dc7fe8a908455fbf91127
Eitan Isaacson <eitani@src.gnome.org>
2009-02-03 14:46:30 +0000

    Provide a $srcdir (bug #570332). Thanks Theppitak Karoonboonyanan!
        * autogen.sh: Provide a $srcdir (bug #570332).
        Thanks Theppitak Karoonboonyanan!
    
    svn path=/trunk/; revision=699

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,7 @@
 # Calls gnome-autogen to build Makefiles and run configure
 
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
 REQUIRED_AUTOMAKE_VERSION=1.7.2
 USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh
