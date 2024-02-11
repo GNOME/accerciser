825470a96722cf81b4fa6cd337c0280c0bf30b4b
Samuel Thibault <samuel.thibault@ens-lyon.org>
2024-02-11 21:03:34 +0100

    Drop last occurrence of importing imp
    Fixes #61

--- pyreqs.py
+++ pyreqs.py
@@ -11,7 +11,7 @@ available under the terms of the BSD License which accompanies
 this distribution, and is available at
 U{http://www.opensource.org/licenses/bsd-license.php}
 '''
-import sys, os, imp
+import sys, os
 
 PYGTK_REQ = '2.0'
 PYATSPI_REQ = (2, 23, 3)
