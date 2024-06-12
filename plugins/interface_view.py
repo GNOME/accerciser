2e8e21f23b8b48a42df8c2b8acb7b44ae7c671f3
Michael Weghorn <m.weghorn@posteo.de>
2024-06-12 10:04:02 +0200

    interface_view: Add help text
    Show help text in the interface view. That property
    was added in at-spi2-core commit [1]
    
        commit 8f1c70155b4132de2c567b3034e4f242e0450df5
        Author: Mike Gorse <mgorse@suse.com>
        Date:   Wed Jan 3 16:52:31 2024 -0600
    
            Add HelpText property and corresponding functions in atk and libatspi.
    
            Closes #146
    
    Currently, at least ATK doesn't notify about changes of
    the help text, so updating it in the interface view
    automatically when it changes while the object is selected
    is not possible as of now.
    
    (I plan to submit a MR to at-spi2-core to implement
    sending such a notification in ATK and will submit
    another change to Accerciser to handle the update
    if that gets accepted.)
    
    [1] https://gitlab.gnome.org/GNOME/at-spi2-core/-/commit/8f1c70155b4132de2c567b3034e4f242e0450df5

--- plugins/interface_view.py
+++ plugins/interface_view.py
@@ -311,6 +311,7 @@ class _SectionAccessible(_InterfaceSection):
     # Child count and description labels
     self.child_count_label = ui_xml.get_object('label_acc_child count')
     self.desc_label = ui_xml.get_object('label_acc_desc')
+    self.help_text_label = ui_xml.get_object('label_acc_help_text')
     self.id_label = ui_xml.get_object('label_acc_id')
 
     # configure states tree view
@@ -343,6 +344,10 @@ class _SectionAccessible(_InterfaceSection):
 
     self.child_count_label.set_text(str(acc.childCount))
     self.desc_label.set_label(acc.description or _('(no description)'))
+    try:
+        self.help_text_label.set_label(acc.get_help_text() or _('(no help text)'))
+    except:
+        self.help_text_label.set_label(_('(no help text)'))
     try:
         self.id_label.set_label(acc.accessibleId)
     except:
